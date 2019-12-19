f3a4864c02a719f3f8d6ff90a9408727078dbe06
Nhân Nguyễn <Goldenream1503@gmail.com>
2019-12-19 21:44:39 +0700

    Init project

---
+++ server/src/main/java/com/nhannn/generic_ecom/GenericEcomApplication.java
@@ -0,0 +1,13 @@
+package com.nhannn.generic_ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericEcomApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/nhannn/generic_ecom/GenericEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhannn.generic_ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
